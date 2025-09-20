d3bcaf66219cd1f8e09b7a3c2f0792b7b2262d05
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-09-20 09:10:57 +0700

    Add Prettier configuration, Header component skeleton, and custom scroll handling hook
    - Created a Prettier configuration file (.prettierrc) with specified formatting options.
    - Added a skeleton for the Header component in Header.jsx.
    - Implemented a custom hook (useScrollHandling) to track scroll direction and position.

---
+++ src/hooks/useScrollHandling.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react';
+const useScrollHandling = () => {
+    const [scrollDriction, setScrollDriction] = useState(null);
+    const previousScrollPosition = useRef(0);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const scrollTracking = () => {
+        const currentScrollPosition = window.pageYOffset; //vị trí hiện tại of scroll
+        if (currentScrollPosition > previousScrollPosition.current) {
+            setScrollDriction('down');
+        } else if (currentScrollPosition < previousScrollPosition.current) {
+            setScrollDriction('up');
+        }
+
+        previousScrollPosition.current =
+            currentScrollPosition <= 0 ? 0 : currentScrollPosition;
+
+        setScrollPosition(currentScrollPosition);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollTracking);
+        return () => window.removeEventListener('scroll', scrollTracking);
+    }, []);
+
+    return {
+        scrollPosition,
+        scrollDriction
+    };
+};
+
+export default useScrollHandling;
